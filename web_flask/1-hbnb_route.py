22c2bbedc950666803dbac22b30bb1c8dd042f74
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-23 12:59:53 +0300

    fixed styling

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,12 @@ def home():
     """ returns a string """
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ returns a string """
     return ("HBNB")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,15 +13,18 @@ def home():
     """ returns a string """
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ returns a string """
     return ("HBNB")
 
+
 @app.route("/c/text", strict_slashes=False)
 def c_with_text():
     """ returns C with text formatted """
     return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
