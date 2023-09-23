753c8dc9d58ea87b63909b5f44d83cd331640ddf
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-23 13:47:31 +0300

    addition to script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''
+script that starts Flask web application
+'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """ returns a string """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ returns a string """
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_with_text(text):
+    """ returns C with text formatted """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ returns python with text formatted """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_is_number(n):
+    """ returns a number is input is integer """
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
