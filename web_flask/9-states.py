5cafa7426bb72f2c6e597918de555189d9ecbca9
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-24 15:43:14 +0300

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays html with States """
+    states = storage.all(State)
+    return render_template('9-states_list.html', states=states, mode='all')
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_by_id(id):
+    """Displays html with States/cities """
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Removes current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
